f7c43a89a8511e3b12c62fa62cdb4a42ebd28df7
Fernando-du <fernan.martineza@duocuc.cl>
2025-11-25 22:17:11 -0300

    Commit conexion

--- src/main/java/com/backendLevelup/Backend/config/SecurityConfig.java
+++ src/main/java/com/backendLevelup/Backend/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -18,24 +17,14 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> {}) // Habilita la configuración de CORS del WebConfig
                 .authorizeHttpRequests(auth -> auth
-                        // Rutas públicas de negocio (ACTUALIZADO A V2)
-                        .requestMatchers("/api/v2/productos/**").permitAll()
-                        .requestMatchers("/api/v2/categorias/**").permitAll()
-                        .requestMatchers("/api/v2/auth/**").permitAll()
-
-                        // Rutas OBLIGATORIAS para que funcione Swagger UI
-                        .requestMatchers("/v3/api-docs/**").permitAll()
-                        .requestMatchers("/swagger-ui/**").permitAll()
-                        .requestMatchers("/swagger-ui.html").permitAll()
-
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 );
+
         return http.build();
     }
 }
+
