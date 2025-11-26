d586a5f3bb044f48a95d4ff9d6e0366963c6403b
Renato416 <re.rojasc@duocuc.cl>
2025-11-25 21:33:02 -0300

    Commit Solucion error boleta

--- src/main/java/com/backendLevelup/Backend/controller/BoletaController.java
+++ src/main/java/com/backendLevelup/Backend/controller/BoletaController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/v2/boletas")
--- src/main/java/com/backendLevelup/Backend/dtos/BoletaDTO.java
+++ src/main/java/com/backendLevelup/Backend/dtos/BoletaDTO.java
@@ -15,4 +15,14 @@ public class BoletaDTO {
     private Long total;
 
     private List<BoletaDetalleDTO> detalles;
+
+    // --- AGREGADO MANUALMENTE PARA CORREGIR EL ERROR ---
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    // ---------------------------------------------------
 }
