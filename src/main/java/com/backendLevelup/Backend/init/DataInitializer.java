448462fe69b0141fadeffd1c4b25f6dbccc9a319
Fernando-du <fernan.martineza@duocuc.cl>
2026-01-21 23:23:41 -0300

    Commit datos

--- src/main/java/com/backendLevelup/Backend/init/DataInitializer.java
+++ src/main/java/com/backendLevelup/Backend/init/DataInitializer.java
@@ -73,7 +73,7 @@ public class DataInitializer {
                     null,
                     "Audífonos Gamer",
                     64990L,
-                    "https://res.cloudinary.com/demo/image/upload/audifonos.jpg",
+                    "https://http2.mlstatic.com/D_NQ_NP_908575-MLA45640430254_042021-F.jpg",
                     "Audífonos gamer con sonido envolvente 7.1",
                     4.5
             );
@@ -82,7 +82,7 @@ public class DataInitializer {
                     null,
                     "Silla Gamer",
                     72990L,
-                    "https://res.cloudinary.com/demo/image/upload/silla.jpg",
+                    "https://images.contentstack.io/v3/assets/blt7c5c2f2f888a7cc3/blt97d26f07c0df0db1/66ba2279f8d31f76f2b63929/Silla_gamer.jpg",
                     "Silla ergonómica reclinable",
                     4.7
             );
@@ -91,7 +91,7 @@ public class DataInitializer {
                     null,
                     "Teclado Mecánico RGB",
                     15990L,
-                    "https://res.cloudinary.com/demo/image/upload/teclado.jpg",
+                    "https://http2.mlstatic.com/teclado-mecanico-gamer-iluminado-rgb-anti-ghosting-yeyian-fl-D_NQ_NP_931450-MLM31237131039_062019-F.jpg",
                     "Teclado mecánico switches azules",
                     4.3
             );
@@ -100,12 +100,29 @@ public class DataInitializer {
                     null,
                     "Mouse Gamer",
                     28990L,
-                    "https://res.cloudinary.com/demo/image/upload/mouse.jpg",
+                    "https://www.toptecnouy.com/imgs/productos/productos34_31059.jpg",
                     "Mouse gamer 16000 DPI",
                     4.4
             );
+            Producto p5 = new Producto(
+                    null,
+                    "Monitor Gamer 27\"",
+                    199990L,
+                    "https://www.hd-tecnologia.com/imagenes/articulos/2017/04/Nuevos-Monitores-Gamers-Predator-de-Acer-con-tecnolog%C3%ADa-Quantum-Dot-Curvo-G-Sync-144-Hz-HDR-y-4k.jpg",
+                    "Monitor 27 pulgadas, 144Hz, resolución QHD",
+                    4.6
+            );
 
-            productoRepository.saveAll(List.of(p1, p2, p3, p4));
+            Producto p6 = new Producto(
+                    null,
+                    "Mousepad XL RGB",
+                    19990L,
+                    "https://i.etsystatic.com/40866174/r/il/09ee67/5156855587/il_1080xN.5156855587_c781.jpg",
+                    "Mousepad extra grande con iluminación RGB",
+                    4.2
+            );
+
+            productoRepository.saveAll(List.of(p1, p2, p3, p4, p5, p6));
 
             System.out.println("✅ Datos iniciales cargados correctamente.");
         };
